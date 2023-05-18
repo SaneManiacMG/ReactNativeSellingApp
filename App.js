5a1e23af62769a8651b7a17213a3ee40672881b1
SaneManiacMG <mmoroane@hotmail.com>
2023-05-18 10:19:09 +0200

    Init Commit

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={require("./assets/background.jpg")}
+        style={styles.imageBackground}
+        resize="cover"
+      />
+      <Image
+        source={require("./assets/logo-red.png")}
+        style={styles.logoImage}
+      />
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={[styles.startPageButton, { backgroundColor: "blue" }]}
+        >
+          <Text style={styles.bigButtonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.startPageButton, { backgroundColor: "purple" }]}
+        >
+          <Text style={styles.bigButtonText}>Register</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +32,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  imageBackground: {
+    flex: 1,
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    width: "100%",
+    height: "100%",
+  },
+  logoImage: {
+    width: 125,
+    height: 125,
+    top: "-25%",
+  },
+  startPageButton: {
+    width: "100%",
+    height: 100,
+    alignItems: "center",
+    justifyContent: "center",
+    color: "white",
+  },
+  buttonContainer: {
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+  },
+  bigButtonText: {
+    color: "white",
+    fontSize: 25,
   },
 });
